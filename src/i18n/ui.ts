333229ec8ea88acbb78c77510c42a108f3446200
jesus <jesus_lopez1997@hotmail.com>
2024-05-10 22:55:34 -0400

    add: ui translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,11 +7,117 @@ export const defaultLang = 'en';
 
 export const ui = {
     en: {
+        'shared.getStarted': 'GET STARTED',
+
         'nav.home': 'Home',
         'nav.about': 'About',
+        'nav.mission': 'Mission',
+        'nav.vision': 'Vision',
+        'nav.roadmap': 'Roadmap',
+
+        'hero.title': 'Discover, Collect and Sell Exclusive Digital Art of NFTs',
+        'hero.subtitle': 'Explore the captivating world of NFTs, where each token represents a unique digital asset, and trust in verified ownership as you collect these one-of-a-kind treasures.',
+
+        'about.title': 'ABOUT',
+        'about.body': 'Tiento is an organization that wants to build a community that unites all who feel genuine passion for football through the combination of  digital collectibles/currency built on  blockchain technologies, and formation/representation rights of professional football players.',
+
+        'mission.title': 'MISSION',
+        'mission.body1': 'Use blockchain technology to close the existing gap between professional football players and fans. This will be done with activating the ability of the general public to invest in the development of players and also have a share in their professional representation.',
+        'mission.body2': 'The blockchain will validate financial transactions, players representation rights ownership through Tiento Sport Management Agency, and the investors earning projections if players increase their market value.',
+
+        'vision.title': 'VISION',
+        'vision.body1': 'Currently, Tiento Sports Agency has a total of 5 professional players. The short term vision is to be able to prove the business plan using these players to provide an investment opportunity using formation and representation rights.',
+        'vision.body2': 'The long term vision is to expand the quantity of players for representation and also hold deals with formation academies in order to expand formation investments of players we don\'t possess representations rights of.',
+
+        'roadmap.title': 'ROADMAP',
+        'roadmap.phase1': 'PHASE 1',
+        'roadmap.phase2': 'PHASE 2',
+        'roadmap.phase3': 'PHASE 3',
+        'roadmap.phase4': 'PHASE 4',
+        'roadmap.phase5': 'PHASE 5',
+        'roadmap.phase6': 'PHASE 6',
+        'roadmap.phase1.name': 'Brand Awareness & Community Incubation',
+        'roadmap.phase2.name': 'Pre-Launching campaign',
+        'roadmap.phase3.name': 'Minting',
+        'roadmap.phase4.name': 'Player Development Program',
+        'roadmap.phase5.name': 'Awareness Platform',
+        'roadmap.phase6.name': 'Holder Utilities',
+
+        'team.title': 'TEAM',
+
+        'partners.title': 'PARTNERS',
+
+        'join.title': 'Start your Tiento journey',
+        'join.subtitle': 'Join our community',
+
+        'footer.motto': 'Helping players to grow in their carrers',
+        'footer.company': 'Company',
+        'footer.company.about': 'About us',
+        'footer.company.carrers': 'Carrers',
+        'footer.company.termsOfService': 'Terms of Service',
+        'footer.company.privacyPolicy': 'Privacy Policy',
+        'footer.resources': 'Resources',
+        'footer.resources.blog': 'Blog',
+        'footer.resources.contact': 'Contact',
+        'footer.resources.community': 'Community',
+        'footer.copyright': '© Copyright Tiento 2024. All Rights Reserved',
     },
     es: {
+        'shared.getStarted': 'EMPIEZA YA',
+
         'nav.home': 'Home',
-        'nav.about': 'Sobre nosotros',
+        'nav.about': 'Sobre Nosotros',
+        'nav.mission': 'Misión',
+        'nav.vision': 'Visión',
+        'nav.roadmap': 'Hoja de ruta',
+
+        'hero.title': 'Descubra, coleccione y venda arte digital exclusivo de NFT',
+        'hero.subtitle': 'Explore el cautivador mundo de las NFT, donde cada ficha representa un activo digital único, y confíe en la propiedad verificada mientras colecciona estos tesoros únicos.',
+
+        'about.title': 'ABOUT',
+        'about.body': 'Tiento es una organización que quiere construir una comunidad que una a todos los que sienten auténtica pasión por el fútbol a través de la combinación de coleccionables/moneda digitales construidos sobre tecnologías blockchain, y derechos de formación/representación de futbolistas profesionales.',
+
+        'mission.title': 'MISIÓN',
+        'mission.body1': 'Utilizar la tecnología blockchain para cerrar la brecha existente entre los futbolistas profesionales y los aficionados. Esto se hará con la activación de la capacidad del público en general para invertir en el desarrollo de los jugadores y también tener una participación en su representación profesional.',
+        'mission.body2': 'La blockchain validará las transacciones financieras, la propiedad de los derechos de representación de los jugadores a través de Tiento Sport Management Agency, y las previsiones de ingresos de los inversores si los jugadores aumentan su valor de mercado.',
+
+        'vision.title': 'VISIÓN',
+        'vision.body1': 'Actualmente, la Agencia Deportiva Tiento cuenta con un total de 5 jugadores profesionales. La visión a corto plazo es poder probar el plan de negocio utilizando estos jugadores para ofrecer una oportunidad de inversión utilizando derechos de formación y representación.',
+        'vision.body2': 'La visión a largo plazo es ampliar la cantidad de jugadores para representación y también celebrar acuerdos con academias de formación para ampliar las inversiones en formación de jugadores de los que no poseemos derechos de representación.',
+
+        'roadmap.title': 'HOJA DE RUTA',
+        'roadmap.phase1': 'FASE 1',
+        'roadmap.phase2': 'FASE 2',
+        'roadmap.phase3': 'FASE 3',
+        'roadmap.phase4': 'FASE 4',
+        'roadmap.phase5': 'FASE 5',
+        'roadmap.phase6': 'FASE 6',
+        'roadmap.phase1.name': 'Notoriedad de Marca e Incubación de Comunidades',
+        'roadmap.phase2.name': 'Campaña de Prelanzamiento',
+        'roadmap.phase3.name': 'Minteado',
+        'roadmap.phase4.name': 'Programa de Desarrollo de Jugadores',
+        'roadmap.phase5.name': 'Plataforma de Concienciación',
+        'roadmap.phase6.name': 'Utilidades del Poseedor',
+
+        'team.title': 'EQUIPO',
+
+        'partners.title': 'SOCIOS',
+
+        'join.title': 'Comienza Tu Aventura Tiento',
+        'join.subtitle': 'Unete a nuestra comunidad',
+
+        'footer.motto': 'Ayudando a jugadores a crecer en sus carreras',
+
+        'footer.company': 'Compañia',
+        'footer.company.about': 'Sobre Nosotros',
+        'footer.company.carrers': 'Empleos',
+        'footer.company.termsOfService': 'Terminos de servicio',
+        'footer.company.privacyPolicy': 'Politica de Privacidad',
+
+        'footer.resources': 'Recursos',
+        'footer.resources.blog': 'Blog',
+        'footer.resources.contact': 'Contacto',
+        'footer.resources.community': 'Comunidad',
+        'footer.copyright': '© Copyright Tiento 2024. Todos los Derechos Reservados',
     },
 } as const;
